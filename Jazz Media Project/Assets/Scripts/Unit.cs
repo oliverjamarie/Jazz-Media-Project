4289cc69b4b869f9822fa6ce909e3b3e1b1e1173
Oliver Marie <oliverjamarie@gmail.com>
2020-11-22 23:14:07 +0100

    Update Unit.cs

--- Jazz Media Project/Assets/Scripts/Unit.cs
+++ Jazz Media Project/Assets/Scripts/Unit.cs
@@ -5,13 +5,38 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
    public string unitName;
+
    public int maxHP;
    public int currHP;
+
    public int maxStamina;
    public int currStamina;
+
+   public int numMoves;
+   public int numMovesRemaining;
+
+   public bool isTurn;
+
+
+   private int countPrint = 0;
+
+   public enum unitTypeEnum{
+      Player,
+      Creature,
+      Enemy
+   }
+
+   public unitTypeEnum unitType;
+
+   private enum actions{
+      Attack,
+      Taunt,
+      Defend
+   }
 
    void Start(){
       currHP = maxHP;
       currStamina = maxStamina;
    }
+
 }
