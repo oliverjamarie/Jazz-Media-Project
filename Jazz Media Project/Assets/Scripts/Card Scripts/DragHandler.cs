0344f5df24a77452970fa7edb5dccaaac1eb1e38
Oliver Marie <oliverjamarie@gmail.com>
2021-02-11 16:50:24 +0100

    Create DragHandler.cs
    Handles dragging a card

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/DragHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //print("Begin Drag");
+
+        parentToReturnTo = transform.parent;
+        transform.SetParent(transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //print("Dragging");
+
+        this.transform.position = eventData.position;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //print("End Drag");
+
+        transform.SetParent(parentToReturnTo);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
