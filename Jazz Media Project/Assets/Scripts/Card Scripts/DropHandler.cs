570a55a446b7844ce8c87e2dfcfbdb9cc3743a1c
Oliver Marie <oliverjamarie@gmail.com>
2021-02-11 16:48:26 +0100

    Create DropHandler.cs
    Handles Dropping the cards into panels (i.e Hand, TableTop)

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/DropHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        print(eventData.pointerDrag.name + " has been dropped onto " + gameObject.name);
+
+        DragHandler drag = eventData.pointerDrag.GetComponent<DragHandler>();
+
+        if (drag != null)
+        {
+            drag.parentToReturnTo = this.transform;
+        }
+    }
+}
