1ed6b8f2b5c85645cf52ed6e275a6d8bea5a9996
Oliver Marie <oliverjamarie@gmail.com>
2021-02-14 23:46:00 +0100

    Create BasicDefend.cs

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/BasicDefend.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicDefend : MonoBehaviour, CardInterface
+{
+    public int defenseValue, cost = 1;
+    public Text cardTitle, cardDesc;
+    BattleManager battleManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        battleManager = GetComponent<BasicCardInfo>().battleManager;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void effect()
+    {
+        Unit player = battleManager.playerUnit;
+
+        player.defense = defenseValue;
+
+        player.numMovesRemaining -= cost;
+    }
+
+    public Text getCardTitle()
+    {
+        return cardTitle;
+    }
+
+    public Text getCardDesc()
+    {
+        return cardDesc;
+    }
+
+    public int getCardCost()
+    {
+        return cost;
+    }
+}
