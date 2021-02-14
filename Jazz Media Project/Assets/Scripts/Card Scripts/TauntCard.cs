acad08097e8c5b54b7a61fe9cec3c5550b7af6fa
Oliver Marie <oliverjamarie@gmail.com>
2021-02-14 23:43:51 +0100

    Taunt Card Update

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/TauntCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TauntCard : MonoBehaviour, CardInterface
+{
+    BattleManager battleManager;
+    public int cost;
+    public Text cardDesc, cardTitle;
+
+    void Start()
+    {
+        battleManager = GetComponent<BasicCardInfo>().battleManager;
+    }
+
+    public void effect()
+    {
+        Unit player = battleManager.playerUnit;
+
+        player.maxNumMoves++;
+
+        player.numMovesRemaining -= cost;
+    }
+
+    public int getCardCost()
+    {
+        return cost;
+    }
+
+    public Text getCardDesc()
+    {
+        return cardDesc;
+    }
+
+    public Text getCardTitle()
+    {
+        return cardTitle;
+    }
+}
