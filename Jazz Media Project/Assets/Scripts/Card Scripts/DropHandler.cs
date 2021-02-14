e625efe509b0350a15ed9ff5a2e407c6d8919bd5
Oliver Marie <oliverjamarie@gmail.com>
2021-02-14 23:44:12 +0100

    Update DropHandler.cs

--- Jazz Media Project/Assets/Scripts/Card Scripts/DropHandler.cs
+++ Jazz Media Project/Assets/Scripts/Card Scripts/DropHandler.cs
@@ -2,18 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DropHandler : MonoBehaviour, IDropHandler
 {
+    public BattleManager battleManager;
+    public Text cardPlayed;
+    string cardPlayedInitialString;
+
+    public void Start()
+    {
+        cardPlayedInitialString = cardPlayed.text;
+        print(cardPlayedInitialString);
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
-        print(eventData.pointerDrag.name + " has been dropped onto " + gameObject.name);
+        bool canPlay = false;
 
         DragHandler drag = eventData.pointerDrag.GetComponent<DragHandler>();
+        CardInterface card = eventData.pointerDrag.GetComponent<CardInterface>();
 
-        if (drag != null)
+
+        if (battleManager.gameState == BattleState.Player_Turn)
+        {
+            canPlay = true;
+        }
+
+        if (drag != null && canPlay)
         {
             drag.parentToReturnTo = this.transform;
+
+            card.effect();
+
+            cardPlayed.text = cardPlayedInitialString + "\t"+
+                card.getCardTitle().text + "\t\t(" + card.getCardDesc().text+")";
+
+            Destroy(eventData.pointerDrag.gameObject);
         }
+
+        battleManager.player.currHandSize--;
     }
 }
