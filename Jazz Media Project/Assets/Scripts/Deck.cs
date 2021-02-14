5b3a99e32eedde2730a23557fa6b516440d7da9e
Oliver Marie <oliverjamarie@gmail.com>
2021-02-14 23:43:04 +0100

    Deck.cs Creation

---
+++ Jazz Media Project/Assets/Scripts/Deck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deck : MonoBehaviour 
+{
+    public List<GameObject> cards = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject gameObject in cards)
+        {
+            if (gameObject.GetComponent<CardInterface>() == null)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+
+    public GameObject dealCard()
+    {
+        int rand = (int)(Random.value * 100) % cards.Count;
+
+        return cards[rand];
+    }
+
+    public GameObject[] dealCards(int numCards)
+    {
+        List<GameObject> dealtCards = new List<GameObject>();
+
+        for (int i = 0; i < numCards; i++)
+        {
+            dealtCards.Add(dealCard());
+        }
+
+        return dealtCards.ToArray();
+    }
+}
