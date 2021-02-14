8926a7bd4b32eebf46a3860f6dd50e110d0a1689
Oliver Marie <oliverjamarie@gmail.com>
2021-02-14 23:41:36 +0100

    Rename Attack.cs to BasicAttack.cs

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/BasicAttack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicAttack : MonoBehaviour, CardInterface
+{
+    public int damage = 1;
+    public int cost = 1;
+    BattleManager battleManager;
+    public Text cardTitle, cardDesc;
+
+    private void Start()
+    {
+        battleManager = GetComponent<BasicCardInfo>().battleManager;
+    }
+
+    public void effect()
+    {
+        battleManager.enemyUnit.takeDamage(damage);
+        battleManager.playerUnit.numMovesRemaining -= cost;
+    }
+
+    public Text getCardTitle()
+    {
+        print(cardTitle.text);
+        return cardTitle;
+    }
+
+    public Text getCardDesc()
+    {
+        return cardDesc;
+    }
+
+    public int getCardCost()
+    {
+        return cost;
+    }
+
+}
