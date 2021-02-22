5ed9a1e0f2be7ae0633085d7c923e99dfd9798cb
Oliver Marie <oliverjamarie@gmail.com>
2021-02-22 14:45:50 +0100

    Update Deck.cs
    Now behaves like an actual deck (deck and discard pile are implemented as stacks)

--- Jazz Media Project/Assets/Scripts/Deck.cs
+++ Jazz Media Project/Assets/Scripts/Deck.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class Deck : MonoBehaviour 
 {
     public List<GameObject> cards = new List<GameObject>();
+    Stack<GameObject> deck = new Stack<GameObject>();
+    Stack<GameObject> discardPile = new Stack<GameObject>();
+
+    public int maxLength;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +21,13 @@ public class Deck : MonoBehaviour
                 throw new System.NotImplementedException();
             }
         }
+
+        initDeck();
     }
 
     public GameObject dealCard()
     {
-        int rand = (int)(Random.value * 100) % cards.Count;
-
-        return cards[rand];
+        return deck.Pop();
     }
 
     public GameObject[] dealCards(int numCards)
@@ -36,5 +40,54 @@ public class Deck : MonoBehaviour
         }
 
         return dealtCards.ToArray();
+    }
+
+    // Initialises the deck stack by inserting all cards in a random order
+    void initDeck()
+    {
+        List<GameObject> cardsCopy = new List<GameObject>(cards);
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            int randIndex = (int)(Random.value * 100) % cardsCopy.Count;
+
+            deck.Push(cardsCopy[randIndex]);
+
+            cardsCopy.RemoveAt(randIndex);
+        }
+    }
+
+    // reinserts the discard pile into the deck and shuffles the deck
+    // Can't use initDeck() function because we need to take into account the
+    // player's cards in hand
+    public void shuffle()
+    {
+        List<GameObject> cardsToShuffle = new List<GameObject>();
+        cardsToShuffle.AddRange(deck);
+        cardsToShuffle.AddRange(discardPile);
+
+        deck.Clear();
+        discardPile.Clear();
+
+        int size = cardsToShuffle.Count;
+
+        for (int i = 0; i < size; i++)
+        {
+            int randIndex = (int)(Random.value * 100) % cardsToShuffle.Count;
+
+            deck.Push(cardsToShuffle[randIndex]);
+
+            cardsToShuffle.RemoveAt(randIndex);
+        }
+    }
+
+    public bool discardCard(GameObject card)
+    {
+        if (card.GetComponent<CardInterface>() == null)
+            return false;
+
+        discardPile.Push(card);
+
+        return true;
     }
 }
