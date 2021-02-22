a677b646e0ab0ceb7fcab9a8998b5d9042aff1a7
Oliver Marie <oliverjamarie@gmail.com>
2021-02-22 14:38:56 +0100

    Prepared Attack Card

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/PreparedAttackCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreparedAttackCard : MonoBehaviour, CardInterface
+{
+    BattleManager battleManager;
+    public int cost = 1;
+    public Text title, description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        battleManager = GetComponent<BasicCardInfo>().battleManager;
+    }
+
+    public void effect()
+    {
+        battleManager.playerUnit.attackPts++;
+        battleManager.playerUnit.numMovesRemaining -= cost;
+    }
+
+    public int getCardCost()
+    {
+        return cost;
+    }
+
+    public Text getCardDesc()
+    {
+        return description;
+    }
+
+    public Text getCardTitle()
+    {
+        return title;
+    }
+
+    
+}
