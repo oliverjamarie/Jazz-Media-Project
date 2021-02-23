6993f7a557ac1f5fda9f2e72cf965d88b89c6742
Oliver Marie <oliverjamarie@gmail.com>
2021-02-23 19:12:44 +0100

    Directory ReOrg

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/Interaction Handling/DragHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    //public BattleManager battleManager;
+    public Transform parentToReturnTo = null;
+    public Transform parentHand; 
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //print("Begin Drag");
+
+        parentToReturnTo = transform.parent;
+        transform.SetParent(transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //print("Dragging");
+
+        this.transform.position = eventData.position;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //print("End Drag");
+
+        transform.SetParent(parentToReturnTo);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/Interaction Handling/DropHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public BattleManager battleManager;
+    public Text cardPlayed;
+    string cardPlayedInitialString;
+
+    public void Start()
+    {
+        cardPlayedInitialString = cardPlayed.text;
+        print(cardPlayedInitialString);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        bool canPlay = false;
+
+        DragHandler drag = eventData.pointerDrag.GetComponent<DragHandler>();
+        CardInterface card = eventData.pointerDrag.GetComponent<CardInterface>();
+
+
+        if (battleManager.gameState == BattleState.Player_Turn)
+        {
+            canPlay = true;
+        }
+
+        if (drag != null && canPlay)
+        {
+
+            if (battleManager.player.playCard(eventData.pointerDrag.gameObject) == true)
+            {
+                cardPlayed.text = cardPlayedInitialString + "\t" +
+                    card.getCardTitle().text + "\t\t(" + card.getCardDesc().text + ")";
+
+                Destroy(eventData.pointerDrag.gameObject);
+
+                drag.parentToReturnTo = this.transform;
+            }
+            else
+            {
+                cardPlayed.text = "Cannot play " + card.getCardTitle();
+            } 
+            
+        }
+
+        // battleManager.player.currHandSize--;
+    }
+}
