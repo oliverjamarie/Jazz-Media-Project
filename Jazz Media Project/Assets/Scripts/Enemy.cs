a3972e9ef1730ff0b77e006ed1d69fa9a7bc2b3d
Oliver Marie <oliverjamarie@gmail.com>
2021-02-23 19:12:50 +0100

    Create Enemy.cs

---
+++ Jazz Media Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Enemy : MonoBehaviour
+{
+    public BattleManager battleManager;
+    public int maxHandSize, currHandSize, startHandSize;
+    public Text nextMove;
+    Deck deck;
+    Unit unit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = GetComponent<Deck>();
+        unit = GetComponent<Unit>();
+    }
+
+    public void enemyPlay()
+    {
+
+        GameObject card = deck.dealCard();
+        card.GetComponent<BasicCardInfo>().battleManager = battleManager;
+        GameObject cardGO = Instantiate(card);
+
+        cardGO.GetComponent<CardInterface>().effect(unit, battleManager.playerUnit);
+        Destroy(cardGO);
+    }
+
+    public GameObject getNextMove()
+    {
+        return deck.getNextCard();
+    }
+}
