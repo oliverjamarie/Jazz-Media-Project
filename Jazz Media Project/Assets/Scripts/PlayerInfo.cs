298dbc6915a3a26cb1b730244dc7c4b183de693f
Oliver Marie <oliverjamarie@gmail.com>
2021-03-01 00:24:41 +0100

    Update PlayerInfo.cs

--- Jazz Media Project/Assets/Scripts/PlayerInfo.cs
+++ Jazz Media Project/Assets/Scripts/PlayerInfo.cs
@@ -6,18 +6,23 @@ using UnityEngine.UI;
 public class PlayerInfo : MonoBehaviour
 {
 
-    public Text health, numMoves, defence;
-    string baseHealthStr, baseNumMovesStr , baseDefenceStr;
-    public BattleManager battleManager;
+    public Text health, numMoves, defence, attPts, defPts;
+    string baseHealthStr, baseNumMovesStr , baseDefenceStr, baseAttPtsStr, baseDefPtsStr;
+    BattleManager battleManager;
     Unit unit;
 
     // Start is called before the first frame update
     void Start()
     {
+        battleManager = GameObject.FindGameObjectWithTag("Battle Manager").GetComponent<BattleManager>();
+
         unit = battleManager.playerUnit;
+
         baseHealthStr = health.text;
         baseNumMovesStr = numMoves.text;
         baseDefenceStr = defence.text;
+        baseAttPtsStr = attPts.text;
+        baseDefPtsStr = defPts.text;
 
         if (unit != null)
         {
@@ -25,18 +30,27 @@ public class PlayerInfo : MonoBehaviour
             numMoves.text = baseNumMovesStr + "\n" + unit.numMovesRemaining + "/" + unit.maxNumMoves;
             defence.text = baseDefenceStr + "\n" + unit.defense;
         }
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
         if (unit == null)
         {
             unit = battleManager.playerUnit;
         }
 
+        if (unit.currHP == 0)
+        {
+            enabled = false;
+            return;
+        }
+
         health.text = baseHealthStr + "\n" + unit.currHP + "/" + unit.maxHP + "HP";
         numMoves.text = baseNumMovesStr + "\n" + unit.numMovesRemaining + "/" + unit.maxNumMoves;
         defence.text = baseDefenceStr + "\n" + unit.defense;
+        attPts.text = baseAttPtsStr + "\n" + unit.attackPts;
+        defPts.text = baseDefPtsStr + "\n" + unit.defensePts;
     }
 }
