3c71cbba74e6d7893e3b7e86c027e55eba37ba1d
Oliver Marie <oliverjamarie@gmail.com>
2021-04-08 22:53:14 +0200

    MainMenu Script
    Handles loading to the first level

---
+++ Jazz Media Project/Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Slider progress;
+
+    public void loadScene()
+    {
+        StartCoroutine(loadAsync());
+        progress.value = 0;
+    }
+
+    IEnumerator loadAsync()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync("KitsuneBattle");
+
+        while (op.isDone == false)
+        {
+            progress.value = op.progress;
+            yield return null;
+        }
+    }
+
+}
