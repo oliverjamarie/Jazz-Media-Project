36c109f61db1e01c4749fec543ab48cdda8133a6
Oliver Marie <oliverjamarie@gmail.com>
2021-04-11 21:31:28 +0200

    Kitsune Champ Card

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/CardBehaviours/ChampionCards/KitsuneChampCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitsuneChampCard : Card
+{
+    public GameObject kitsuneChampPrefab;
+
+    public override void effect(Unit playedBy, Unit target)
+    {
+        battleManager.setupChamp(kitsuneChampPrefab);
+    }
+}
