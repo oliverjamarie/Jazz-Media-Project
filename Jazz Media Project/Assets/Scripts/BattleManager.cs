86da493bacd1eb638f7d6b175cbe47bcd1682aeb
Oliver Marie <oliverjamarie@gmail.com>
2021-04-11 21:31:35 +0200

    Update BattleManager.cs

--- Jazz Media Project/Assets/Scripts/BattleManager.cs
+++ Jazz Media Project/Assets/Scripts/BattleManager.cs
@@ -16,32 +16,35 @@ public class BattleManager : MonoBehaviour
 {
 
     public GameObject playerPrefab;
-    public Transform playerSpawnPoint;
     public GameObject playerGO;
+    public GameObject playerHand;
+    public Transform playerSpawnPoint;
+    public Unit playerUnit;
+
 
     public GameObject enemyPrefab;
     public Transform enemySpawnPoint;
     public GameObject enemyGO;
-
-    public GameObject playerHand;
-
-    public BattleState gameState;
-
-    public Unit playerUnit;
     public Unit enemyUnit;
 
-    public Player player;
+    public GameObject champGO;
+    public Transform champSpawnPoint;
+    public Unit champUnit;
+    public bool champInPlay;
+
+    public BattleState gameState;
 
     // Start is called before the first frame update
     void Start()
     {
         gameState = BattleState.Start;
         setupBattle();
-
     }
 
     void Update()
     {
+        champInPlay = (champGO != null);
+
         if (playerUnit.currHP <= 0)
         { // Player is dead
             print(playerUnit.unitName + " is dead");
@@ -56,23 +59,35 @@ public class BattleManager : MonoBehaviour
             enabled = false;
             Destroy(enemyGO);
         }
-        else
+        else if (champInPlay)
         {
-            if (gameState == BattleState.Player_Turn)
-            { // Player's Turn
-                battlePlayerTurn();
+            if (champUnit.currHP <= 0)
+            {
+                print("Champion is dead");
+                Destroy(champGO);
             }
-            else if (gameState == BattleState.Enemy_Turn)
-            {// Enemy's Turn
-                battleEnemyTurn();
+            else if (champUnit.currStamina <= 0)
+            {
+                print("Champion rand out of stamina");
+                Destroy(champGO);
+                champInPlay = false;
             }
+            
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            print("SPACEBAR PRESSED");
-            switchHand();
+        if (gameState == BattleState.Player_Turn)
+        { // Player's Turn
+            battlePlayerTurn();
         }
+        else if (gameState == BattleState.Enemy_Turn)
+        {// Enemy's Turn
+            battleEnemyTurn();
+        }
+        else if (champInPlay && gameState == BattleState.Player_Champ_Turn)
+        {
+            battleChampTurn();
+        }
+
     }
 
     void setupBattle()
@@ -81,16 +96,57 @@ public class BattleManager : MonoBehaviour
         playerPrefab.GetComponent<Player>().handTransform = playerHand;
         playerGO = Instantiate(playerPrefab, playerSpawnPoint);
         playerUnit = playerGO.GetComponent<Unit>();
-        player = playerGO.GetComponent<Player>();
 
         enemyGO = Instantiate(enemyPrefab, enemySpawnPoint);
         enemyUnit = enemyGO.GetComponent<Unit>();
 
         gameState = BattleState.Player_Turn;
+
+        champInPlay = false;
+
+        if (playerHand == null)
+        {
+            playerHand = GameObject.FindGameObjectWithTag("Hand");
+        }
+    }
+
+    public void setupChamp(GameObject champPrefab)
+    {
+        champGO = Instantiate(champPrefab);
+        champUnit = champGO.GetComponent<Unit>();
+        GameObject.FindGameObjectWithTag("ChampUI").GetComponent<UnitUI>().enabled = true;
+        champGO.tag = "Champion";
+        champInPlay = true;
     }
 
     void battlePlayerTurn()
     {
+        Transform alt = GameObject.FindGameObjectWithTag("AlternateHand").transform;
+        GameObject[] playerCards = GameObject.FindGameObjectsWithTag("PlayerCard");
+
+        
+
+        if (champInPlay)
+        {
+            GameObject[] champCards = GameObject.FindGameObjectsWithTag("ChampCard");
+
+            foreach (GameObject card in playerCards)
+            {
+                if (card.transform.IsChildOf(alt))
+                {
+                    card.transform.SetParent(playerHand.transform);
+                }
+            }
+
+            foreach (GameObject card in champCards)
+            {
+                if (card.transform.IsChildOf(playerHand.transform))
+                {
+                    card.transform.SetParent(alt);
+                }
+            }
+        }
+        
 
         if (playerUnit.numMovesRemaining <= 0)
         {
@@ -103,57 +159,74 @@ public class BattleManager : MonoBehaviour
     {
         enemyGO.GetComponent<Enemy>().enemyPlay();
 
-        playerUnit.initTurn();
+        if (champInPlay == false)
+        {
+            playerUnit.initTurn();
 
-        gameState = BattleState.Player_Turn;
+            gameState = BattleState.Player_Turn;
+        }
+        else
+        {
+            print("Champ turn");
+            champUnit.initTurn();
+            gameState = BattleState.Player_Champ_Turn;
+        }
 
+    }
+
+    void battleChampTurn()
+    {
+
+        Transform alt = GameObject.FindGameObjectWithTag("AlternateHand").transform;
+        GameObject[] playerCards = GameObject.FindGameObjectsWithTag("PlayerCard");
+        GameObject[] champCards = GameObject.FindGameObjectsWithTag("ChampCard");
+
+        foreach (GameObject card in playerCards)
+        {
+            if (card.transform.IsChildOf(playerHand.transform))
+            {
+                card.transform.SetParent(alt);
+            }
+        }
+
+        foreach (GameObject card in champCards)
+        {
+            if (card.transform.IsChildOf(alt))
+            {
+                card.transform.SetParent(playerHand.transform);
+            }
+        }
+
+        if (champUnit.numMovesRemaining <= 0)
+        {
+            playerUnit.initTurn();
+            gameState = BattleState.Player_Turn;
+        }
     }
 
     public void DealCardBtnListen()
     {
         if (gameState == BattleState.Player_Turn)
         {
-            if (player == null)
-            {
-                print("PLAYER IS NULL");
-            }
-
-            player.DealCard();
+            playerGO.GetComponent<Player>().DealCard();
         }
     }
 
-    public void switchHand()
+    public Player getPlayer()
     {
         if (gameState == BattleState.Player_Turn)
         {
-            GameObject[] playerCards = GameObject.FindGameObjectsWithTag("PlayerCard");
-            Transform trans = GameObject.FindWithTag("AlternateHand").transform;
-
-            foreach (GameObject obj in playerCards)
-            {
-                obj.transform.SetParent(trans);
-            }
-
-
-            // This is only for testing purposes
-            gameState = BattleState.Player_Champ_Turn;
+            return playerUnit.player;
         }
-        else if (gameState == BattleState.Player_Champ_Turn)
+
+        if (gameState == BattleState.Player_Champ_Turn)
         {
-            GameObject[] playerCards = GameObject.FindGameObjectsWithTag("PlayerCard");
-            Transform trans = GameObject.FindWithTag("Hand").transform;
-            Transform discardPileTrans = GameObject.FindGameObjectWithTag("DiscardPile").transform;
-            foreach (GameObject obj in playerCards)
-            {
-
-                if (obj.transform.IsChildOf(discardPileTrans) == false)
-                    obj.transform.SetParent(trans);
-            }
-
-
-            // This is only for testing purposes
-            gameState = BattleState.Player_Turn;
+            return champUnit.player;
         }
 
+
+        return null;
     }
+
+    
 }
