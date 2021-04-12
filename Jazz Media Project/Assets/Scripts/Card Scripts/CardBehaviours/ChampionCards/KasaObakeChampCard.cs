28148e0cfdb75864151071cd6a4cacdec880ab27
Oliver Marie <oliverjamarie@gmail.com>
2021-04-12 12:23:13 +0200

    Create KasaObakeChampCard.cs

---
+++ Jazz Media Project/Assets/Scripts/Card Scripts/CardBehaviours/ChampionCards/KasaObakeChampCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KasaObakeChampCard : Card
+{
+    public GameObject kasaObakeChampPrefab;
+
+    public override void effect(Unit playedBy, Unit target)
+    {
+        battleManager.setupChamp(kasaObakeChampPrefab);
+    }
+}
